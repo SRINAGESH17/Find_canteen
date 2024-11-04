d83d7c1ea652032e3fac10031e4062778bdf9803
rituraj-skygoal <rituraj@skygoalnext.com>
2024-11-04 12:27:48 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ pincodes = data[['PinCode']]
 kmeans = KMeans(n_clusters=5)  # Specify the number of clusters
 pincodes['Cluster'] = kmeans.fit_predict(pincodes[["PinCode"]])
 data_with_cluster = pd.merge(data, pincodes, on="PinCode")
+data1 = pd.read_csv("Pincode_30052019.csv",encoding="ISO-8859-1")
+data1 = data.drop_duplicates(subset=["Pincode"])
+
+pincode_dict = data.set_index('Pincode')[['Office Name', 'StateName', 'District', 'Division Name']].to_dict(orient='index')
+
+pincode_values = data[['Pincode']].values
+
+knn_model = NearestNeighbors(n_neighbors=1).fit(pincode_values)
 
 def nearby_canteen(zipcode):
     if len(str(zipcode)) != 6:
@@ -28,6 +36,43 @@ def nearby_canteen(zipcode):
             nearest_canteen = data_with_cluster.loc[data_with_cluster['Distance'].idxmin()]
             return nearest_canteen.to_dict()
 
+
+
+@app.route('/get_post_office', methods=['GET'])
+def get_post_office_info():
+    pincode = request.args.get('pincode', type=int)
+    if pincode is None:
+        return jsonify({"error": "Please provide a pincode"}), 400
+
+    if pincode in pincode_dict:
+        result = {
+            "pincode": pincode,
+            "post_office": pincode_dict[pincode]['Office Name'],
+            'District': pincode_dict[pincode]['District'],
+            'Division': pincode_dict[pincode]['Division Name'] ,
+            'State': pincode_dict[pincode]['StateName'],
+            "message": "Pincode found"
+        }
+    else:
+        try:
+            _, indices = knn_model.kneighbors([[pincode]])
+            nearest_pincode = int(data.iloc[indices[0][0]]['Pincode'])
+            result = {
+                "message": "Pincode not found. Nearest serviceable pincode provided.",
+                "nearest_pincode": nearest_pincode,
+                "post_office": pincode_dict[nearest_pincode]['Office Name'],
+                'District': pincode_dict[pincode]['District'],
+                'Division': pincode_dict[pincode]['Division Name'],
+                "state": pincode_dict[nearest_pincode]['State']
+            }
+        except:
+            result = {
+                "message": "Pincode not found. No serviceable pincode found."
+            }
+    try:
+        return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
 @app.route('/nearby-canteen', methods=['GET'])
 def get_nearby_canteen():
     # Get the zipcode from the query parameters
